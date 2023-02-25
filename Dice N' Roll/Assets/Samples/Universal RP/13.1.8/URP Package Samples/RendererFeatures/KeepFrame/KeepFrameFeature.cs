48afce48d448fc8e040e232fdc0f96a67e35a7dd
MakerFaffa <makerfaffa@gmail.com>
2023-02-25 18:35:41 +0100

    Updated to Unity 2023.1.0b5

---
+++ Dice N' Roll/Assets/Samples/Universal RP/13.1.8/URP Package Samples/RendererFeatures/KeepFrame/KeepFrameFeature.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+//This renderer feature will replicate a "don't clear" behaviour by injecting two passes into the pipeline:
+//One pass that copies color at the end of a frame
+//Another pass that draws the content of the copied texture at the beginning of a new frame
